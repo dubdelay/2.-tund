b917ef2611c3967fdd5a578aa59258ea908c748b
alarpenn <alarpenn@greeny.cs.tlu.ee>
2016-09-14 08:53:46 +0300

    first lines of code

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<?php
+	//muutuja ette dolla dolla ja semikoolon lõpetab käsu
+	$firstname = "Alari";
+	$lastname = "Pennar";
+	//echo trükib välja muutuja väärtuse
+	
+	/*
+	echo $firstname;
+	echo $lastname;
+	*/
+	
+	//punkt liidab stringe
+	echo $firstname." ".$lastname;
+?>
