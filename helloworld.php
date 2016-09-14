133d4f9e21e122714ff04e684b2e9760c42fc46e
alarpenn <alarpenn@greeny.cs.tlu.ee>
2016-09-14 09:36:02 +0300

    more lines of code

--- helloworld.php
+++ helloworld.php
@@ -12,3 +12,44 @@
 	//punkt liidab stringe
 	echo $firstname." ".$lastname;
 ?>
+
+<br>
+
+<?php
+
+	$age = 5;
+	
+	if ($age >= 130) 
+	{
+		//kui vastus tõene (true)
+		echo "tatikas";
+	} 
+	else 
+	{
+		//kui vastus väär (false)
+		echo "muld";
+	}
+?>
+
+<br>
+
+<?php
+	/*
+	for (1;2;3)
+	{sisu+n} korda | 1) Enne 1. korda ($i = 0)| 2) Enne igat korda ($i < $age)| 3) Pärast igat korda ($i = $i + n)
+	*/
+	for ($i = 1; $i < $age; $i = $i +1)
+	{
+		echo /*$i.*/ "palju ";
+	}
+	echo "õnne";
+?>
+
+ <br>
+ 
+ <?php
+ 
+	echo date("d/m/Y"). " ning ".date("z"). ". päev aastas.";
+	echo "Kell on ". date("G:s");
+	
+?>
